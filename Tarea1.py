83e7ea3288b25003772f0c0695bd827bdbc1f926
lecastro3 <lecastro3@espe.edu.ec>
2022-05-27 16:56:22 -0500

    Avance 2

---
+++ Tarea1.py
@@ -0,0 +1,20 @@
+#Importar las librerias
+from markupsafe import escape
+# from email.mime import application
+from flask import Flask, abort, render_template
+
+
+#Instanciar la aplicaicon
+app = Flask(__name__)
+
+#Ruta principal
+@app.route('/')
+#Llamar a index.html en la ruta principal
+def principal():
+    return render_template('index.html')
+
+
+#Main del programa
+if __name__ == '__main__':
+    #debug = True, para reiniciar automatica el servidor
+    app.run(debug=True)
